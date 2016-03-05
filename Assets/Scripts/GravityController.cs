03840537c57d2f24c21605f690025b8bcc56d49a
Xkynar <xkynar@gmail.com>
2016-03-05 15:47:32 +0000

    Created gravity system and player character

---
+++ Assets/Scripts/GravityController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityController : MonoBehaviour {
+
+    [SerializeField] private float gravity = -100f;
+    [SerializeField] private Transform defaultAttractor;
+
+    //Utility
+    private Vector3 gravityUp;
+    private Vector3 bodyUp;
+
+    private bool inWarpZone = false;
+
+    // Components
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        rb.gravityScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        //Update vector values
+        gravityUp = (transform.position - defaultAttractor.position).normalized;
+        bodyUp = transform.up;
+
+        //Apply gravity
+        rb.AddForce(getGravity());
+
+        //Rotate correctly
+        Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, gravityUp) * transform.rotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 50 * Time.deltaTime);
+    }
+
+    public Vector2 getGravity()
+    {
+        if (!inWarpZone) //default gravity
+        {
+            return gravityUp * gravity;
+        }
+        else
+        {
+            //TODO
+            return gravityUp * gravity;
+        }
+    }
+       
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    //Params
+    [SerializeField] private LayerMask jumpRayMask;
+    [SerializeField] private float moveSpeed = 10000f;
+    [SerializeField] private float maxVelocity = 10f;
+    [SerializeField] private float jumpForce = 1000f;
+
+    //Utility
+    private float moveAxis;
+    private float jumpAxis;
+    private Vector3 moveVec;
+    private Vector2 moveDir;
+
+    //Components
+    private Rigidbody2D rb;
+
+	
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+	}
+
+    void FixedUpdate()
+    {
+        moveAxis = Input.GetAxisRaw("Horizontal");
+        jumpAxis = Input.GetAxisRaw("Vertical");
+
+        //Handle side movement
+        moveDir = transform.right * moveAxis * moveSpeed * Time.deltaTime;
+
+        float moveSign = transform.InverseTransformDirection(moveDir).x;
+        float velSign = transform.InverseTransformDirection(rb.velocity).x;
+        bool moveDiff = Mathf.Sign(moveSign) != Mathf.Sign(velSign);
+
+        if (rb.velocity.magnitude < maxVelocity || moveDiff)
+        {
+            rb.AddForce(moveDir);
+        }
+
+        //Handle jump
+        if (jumpAxis > 0 && isGrounded())
+        {
+            rb.AddForce(transform.up * jumpForce);
+        }
+         
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.Raycast(transform.position, -transform.up, 2.3f, jumpRayMask);
+    }
+}
